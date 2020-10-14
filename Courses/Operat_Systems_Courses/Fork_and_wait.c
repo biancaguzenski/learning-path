a5bcd28b454cc6efd29d9b010dc6ef6121c6c83a
Bianca <biancalway@gmail.com>
2020-10-14 16:39:05 -0300

    Implemening fork with wait

---
+++ Courses/Operat_Systems_Courses/Fork_and_wait.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+/* This is an exercise is proposed in chapter 5 of the book "Operating Systems: Three Easy Pieces" */
+
+/* The fork() system call and the use of wait() */
+
+int main() {
+    printf("Hello, world (pid:%d)\n", (int) getpid());
+    int rc = fork(); /* store pid */
+    
+    if(rc < 0) {
+        /* fork failed */
+        fprintf(stderr, "Fork failed\n");   
+        exit(1);
+    } else if (rc == 0) {
+        /* new process - child */
+        printf("Hello, I'm a new process! (pid:%d)\n", (int) getpid());
+    } else {
+        /* parent goes down this path */
+        int rc_wait = wait(NULL);
+        printf("Hello, I'm a process-parent of %d (rc_wait:%d) (pid:%d)\n", rc, rc_wait, (int) getpid());
+    }
+    return 0;
+    /* Here we use wait function wait() */
+}
