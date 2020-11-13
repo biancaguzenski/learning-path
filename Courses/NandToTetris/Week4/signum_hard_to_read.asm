ca8b62918dd0d8ed1e283a0f0745de080245efa6
Bianca <biancalway@gmail.com>
2020-11-13 15:31:42 -0300

    First lines in Assembly.

---
+++ Courses/NandToTetris/Week4/signum_hard_to_read.asm
@@ -0,0 +1,25 @@
+// Program: Signum.asm
+// Computes if R0>0
+//		R1=1
+//	    else
+//		R1=0
+// Usage: put a value in R0
+
+@R0
+D=M       // D=RAM[0]
+
+@8        // "opens" instruction in line 8
+D;JGT     // JGT sees if d==0, then goto line 8
+
+@R1
+M=0       // RAM[1]=0
+@10
+0;JMP     // end of program
+
+@R1
+M=1       // R1=1
+
+@10
+0;JMP
+
+//
