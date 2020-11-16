89943f4b661d31fbeca26d0a0eb5c7209d8b9406
Bianca <biancalway@gmail.com>
2020-11-16 16:04:20 -0300

    Week 4 - homework - working with hack assembly.

--- Courses/NandToTetris/Week4/forloop.asm
+++ Courses/NandToTetris/Week4/forloop.asm
@@ -5,46 +5,46 @@
 
 ; first we set the variables
 ; arr = 100
-@100
-D=A
-@arr
-M=D
+	@100
+	D=A
+	@arr
+	M=D
 
 ; n = 10
-@10
-D=A
-@n
-M=D
+	@10
+	D=A
+	@n
+	M=D
 
 ; i = 0
-@i
-M=0
+	@i
+	M=0
 
 (LOOP)
 ; if i==n goto END
 ; access i, access n, if d is equal 0 goes to end
-@i
-D=M
-@n
-D=D-M
-@END
-D;JEQ
+	@i
+	D=M
+	@n
+	D=D-M
+	@END
+	D;JEQ
 
 ; set the value of the current array position to -1
 ; RAM[arr+i] = -1
-@arr
-D=M
-@i
-A=D+M
-M=-1
+	@arr
+	D=M
+	@i
+	A=D+M
+	M=-1
 
 ; i++
-@i
-M=M+1
+	@i
+	M=M+1
 
 @LOOP
-O;JMP
+	O;JMP
 
 (END)
-@END
-0;JMP
+	@END
+	0;JMP
--- Courses/NandToTetris/Week4/mult/mult.asm
+++ Courses/NandToTetris/Week4/mult/mult.asm
@@ -1,8 +1,6 @@
+; Multiplies R0 and R1 and stores the result in R2.
+; (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Multiplies R0 and R1 and stores the result in R2.
-// (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
-
-// Put your code here.
 
 @sum
 M=0
