3cb8e1c2fea7465370f985f971327f0fb1a0fcac
Bianca <biancalway@gmail.com>
2020-11-16 16:00:42 -0300

    Week 4 - homework - working with hack assembly.

--- Courses/NandToTetris/Week4/fill/Fill.asm
+++ Courses/NandToTetris/Week4/fill/Fill.asm
@@ -1,8 +1,3 @@
-// This file is part of www.nand2tetris.org
-// and the book "The Elements of Computing Systems"
-// by Nisan and Schocken, MIT Press.
-// File name: projects/04/Fill.asm
-
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
@@ -12,3 +7,51 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+@status
+    M=-1        ; status = all black
+    D=0         
+    @SETSCREEN
+    0;JMP
+
+(LOOP)
+    @KBD
+    D=M         
+    @SETSCREEN
+    D;JEQ       ; if no key pressed, goto setscreen
+    D=-1        ; if key pressed, black screen
+    
+(SETSCREEN)
+    @ARG
+    M=D         ; arg saves current state
+    @status     ; status of entire screen
+    D=D-M       ; new status
+    @LOOP       ; cme back to loop
+    D;JEQ       ; do nothing if no change
+    
+    @ARG
+    D=M
+    @status
+    M=D         ; set current status
+    
+    @SCREEN
+    D=A         ; screen address
+    @8192
+    D=D+A
+    @i
+    M=D
+    
+(SETLOOP)    
+    @i
+    D=M-1
+    M=D         
+    @LOOP
+    D;JLT       
+    
+    @status
+    D=M         
+    @i
+    A=M         
+    M=D         
+    @SETLOOP
+    0;JMP
