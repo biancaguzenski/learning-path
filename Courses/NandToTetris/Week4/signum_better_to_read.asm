38ef33d9160b6d21c984ac1f922c1ae56e22fcf9
Bianca <biancalway@gmail.com>
2020-11-16 16:09:04 -0300

    Week 4 - homework - working with hack assembly.

--- Courses/NandToTetris/Week4/signum_better_to_read.asm
+++ Courses/NandToTetris/Week4/signum_better_to_read.asm
@@ -7,22 +7,22 @@
 
 ; Positive label
 
-@R0
-D=M
+	@R0
+	D=M
 
-@POSITIVE
-D;JGT
+	@POSITIVE
+	D;JGT
 
-@R1
-M=0
-@END
-0;JMP
+	@R1
+	M=0
+	@END
+	0;JMP
 
 (POSITIVE)
 
-@R1
-M=1
+	@R1
+	M=1
 
 (END)
-@END
-0;JMP
+	@END
+	0;JMP
